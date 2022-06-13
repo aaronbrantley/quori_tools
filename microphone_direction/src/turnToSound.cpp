a0ae496f462ac523ccfc8a47878277a3d1289e95
Aaron Brantley <aarongbrantley@gmail.com>
2022-06-13 14:58:09 -0700

    switch from cmd_vel to move_base

--- microphone_direction/src/turnToSound.cpp
+++ microphone_direction/src/turnToSound.cpp
@@ -1,6 +1,7 @@
 #include "DirectionListener.h"
 
-#include <geometry_msgs/Twist.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
 int main (int argc, char ** argv)
 {
@@ -10,11 +11,13 @@ int main (int argc, char ** argv)
   // ros
   ros::init (argc, argv, "turn_to_sound");
   ros::NodeHandle turnToSoundNode;
-  ros::Publisher turnPublisher = turnToSoundNode.advertise <geometry_msgs::Twist> ("quori/base_controller/cmd_vel", 1);
   ros::Rate loopRate (1);
 
-  //geometry_msgs
-  geometry_msgs::Twist rotationMessage;
+  // actionlib
+  actionlib::SimpleActionClient <move_base_msgs::MoveBaseAction> actionClient ("move_base", true);
+
+  // move_base_msgs
+  move_base_msgs::MoveBaseGoal goal;
 
   // primitive
   int currentSoundDirection;
@@ -25,22 +28,31 @@ int main (int argc, char ** argv)
   {
     currentSoundDirection = soundDirection.getSoundDirection ();
 
-    // if a new sound direction is found
+    // if a new sound direction is published
     if (currentSoundDirection - subtract != 0)
-    { 
+    {
+      // convert degrees to radians
       rotation = currentSoundDirection * 3.14 / 180;
-      
+
       ROS_INFO_STREAM ("turning " << rotation << " rads");
 
-      rotationMessage.linear.x = 0;
-      rotationMessage.linear.y = 0;
-      rotationMessage.linear.z = 0;
+      // goal header
+      goal.target_pose.header.frame_id = "respeaker_base";
+      goal.target_pose.header.stamp = ros::Time::now ();
 
-      rotationMessage.angular.x = 0;
-      rotationMessage.angular.y = 0;
-      rotationMessage.angular.z = rotation;
+      // goal position
+      goal.target_pose.pose.position.x = 0;
+      goal.target_pose.pose.position.y = 0;
+      goal.target_pose.pose.position.z = 0;
 
-      turnPublisher.publish (rotationMessage);
+      // goal orientation
+      goal.target_pose.pose.orientation.x = 0;
+      goal.target_pose.pose.orientation.y = 0;
+      goal.target_pose.pose.orientation.w = rotation;
+
+      // send goal
+      actionClient.sendGoal (goal);
+      actionClient.waitForResult ();
 
       subtract = currentSoundDirection;
     }

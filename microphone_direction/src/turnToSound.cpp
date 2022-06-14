1c76c799f1fc6da3c7bf6da153df7624070f552a
Aaron Brantley <aarongbrantley@gmail.com>
2022-06-14 14:31:27 -0700

    small adjustments to turning

--- microphone_direction/src/turnToSound.cpp
+++ microphone_direction/src/turnToSound.cpp
@@ -9,25 +9,22 @@ int main (int argc, char ** argv)
   // classes
   DirectionListener soundDirection;
 
+  // primitive
+  int currentSoundDirection;
+
   // ros
   ros::init (argc, argv, "turn_to_sound");
   ros::NodeHandle waitForNewSoundNode;
   ros::Rate loopRate (1);
-
-  // primitive
-  int currentSoundDirection;
-  int difference = 0;
 
   while (waitForNewSoundNode.ok ())
   {
     currentSoundDirection = soundDirection.getSoundDirection ();
 
     // if a new sound direction is found
-    if (currentSoundDirection - difference != 0)
+    if (currentSoundDirection != 0)
     {
-      smoothTurn (currentSoundDirection, 10);
-
-      difference = currentSoundDirection;
+      smoothTurn (currentSoundDirection, currentSoundDirection / 10);
     }
 
     ros::spinOnce ();
@@ -39,16 +36,17 @@ int main (int argc, char ** argv)
 
 void smoothTurn (int degrees, int scale)
 {
+  // primitive
+  double rotation;
+  int rate = 5;
+
   // ros
   ros::NodeHandle turnToSoundNode;
-  ros::Rate loopRate (5);
+  ros::Rate loopRate (rate);
   ros::Publisher turnPublisher = turnToSoundNode.advertise <geometry_msgs::Twist> ("quori/base_controller/cmd_vel", 1);
 
   // geometry_msgs
   geometry_msgs::Twist rotationMessage;
-
-  // primitive
-  double rotation;
 
   // convert degrees to radians
   rotation = degrees * 3.14 / 180;
@@ -63,11 +61,11 @@ void smoothTurn (int degrees, int scale)
   rotationMessage.angular.y = 0;
   rotationMessage.angular.z = rotation / scale;
 
-  for (int iterations = 0; iterations < scale * 5; iterations += 1)
+  for (int iterations = 0; iterations < scale * rate; iterations += 1)
   {
     turnPublisher.publish (rotationMessage);
 
-    ROS_INFO_STREAM ("rotation message published");
+    ROS_DEBUG_STREAM ("rotation message published");
 
     ros::spinOnce ();
     loopRate.sleep ();

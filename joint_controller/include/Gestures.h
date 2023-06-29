4dbaa61c51eb9ef13274fac220ea926f91428367
Aaron Brantley <aarongbrantley@gmail.com>
2023-06-29 15:32:57 -0700

    improve wave gesture

--- joint_controller/include/Gestures.h
+++ joint_controller/include/Gestures.h
@@ -39,83 +39,97 @@ class Gesture
 
 class Wave : public Gesture
 {
-private:
-	joint_values raiseArm;
-	joint_values waveArm;
-	joint_values waveArmBack;
-	joint_values lowerArm;
+	private:
+		joint_values raiseArm;
+		joint_values waveArm;
+		joint_values waveArmBack;
+		joint_values lowerArm;
 
-public:
+	public:
 
-	/*
-	* 	waves the left arm
-	*/
-	Wave ()
-	{
-		raiseArm.leftShoulderPitch = -0.25;
-		raiseArm.leftShoulderRoll = 1.0;
-		waveArm.leftShoulderPitch = -0.25;
-		waveArm.leftShoulderRoll = 0.75;;
-		waveArmBack.leftShoulderPitch = -0.25;
-		waveArmBack.leftShoulderRoll = 1.0;
-		lowerArm.leftShoulderPitch = -2.3;
-		lowerArm.leftShoulderRoll = 1.0;
-
-		raiseArm.speed = 1.0;
-		waveArm.speed = 0.25;
-		waveArmBack.speed = 0.25;
-		lowerArm.speed = 1.0;
-
-		addPose (raiseArm);
-		addPose (waveArm);
-		addPose (waveArmBack);
-		addPose (waveArm);
-		addPose (waveArmBack);
-		addPose (lowerArm);
-	}
-
-	/*
-	* 	choose which arm to wave
-	* 	true for left arm
-	* 	false for right arm
-	*/
-	Wave (bool arm)
-	{
-		if (arm)
+		/*
+		* 	waves the left arm
+		*/
+		Wave ()
 		{
-			raiseArm.leftShoulderPitch = -0.25;
-			raiseArm.leftShoulderRoll = 1.0;
-			waveArm.leftShoulderPitch = -0.25;
-			waveArm.leftShoulderRoll = 0.75;
-			waveArmBack.leftShoulderPitch = -0.25;
-			waveArmBack.leftShoulderRoll = 1.0;
-			lowerArm.leftShoulderPitch = -2.3;
-			lowerArm.leftShoulderRoll = 1.0;
-		}
-		else
-		{
-			raiseArm.rightShoulderPitch = -0.25;
-			raiseArm.rightShoulderRoll = 1.0;
-			waveArm.rightShoulderPitch = -0.25;
-			waveArm.rightShoulderRoll = 0.75;
-			waveArmBack.rightShoulderPitch = -0.25;
-			waveArmBack.rightShoulderRoll = 1.0;
-			lowerArm.rightShoulderPitch = -2.3;
-			lowerArm.rightShoulderRoll = 1.0;
+			raiseArm.leftShoulderPitch = 2.3;
+			raiseArm.leftShoulderRoll = -1.0;
+			raiseArm.rightShoulderRoll = -1.0;
+			waveArm.leftShoulderPitch = 2.3;
+			waveArm.leftShoulderRoll = -0.5;
+			waveArm.rightShoulderRoll = -1.0;
+			waveArmBack.leftShoulderPitch = 2.3;
+			waveArmBack.leftShoulderRoll = -1.0;
+			waveArmBack.rightShoulderRoll = -1.0;
+			lowerArm.leftShoulderPitch = 0.0;
+			lowerArm.leftShoulderRoll = -1.0;
+			lowerArm.rightShoulderRoll = -1.0;
+
+			raiseArm.speed = 1.0;
+			waveArm.speed = 0.25;
+			waveArmBack.speed = 0.25;
+			lowerArm.speed = 1.0;
+
+			addPose (raiseArm);
+			addPose (waveArm);
+			addPose (waveArmBack);
+			addPose (waveArm);
+			addPose (waveArmBack);
+			addPose (lowerArm);
 		}
 
-		raiseArm.speed = 1.0;
-		waveArm.speed = 0.25;
-		waveArmBack.speed = 0.25;
-		lowerArm.speed = 1.0;
+		/*
+		* 	choose which arm to wave
+		* 	true for left arm
+		* 	false for right arm
+		*/
+		Wave (bool arm)
+		{
+			// left arm
+			if (arm)
+			{
+				raiseArm.leftShoulderPitch = 2.3;
+				raiseArm.leftShoulderRoll = -1.0;
+				raiseArm.rightShoulderRoll = -1.0;
+				waveArm.leftShoulderPitch = 2.3;
+				waveArm.leftShoulderRoll = -0.5;
+				waveArm.rightShoulderRoll = -1.0;
+				waveArmBack.leftShoulderPitch = 2.3;
+				waveArmBack.leftShoulderRoll = -1.0;
+				waveArmBack.rightShoulderRoll = -1.0;
+				lowerArm.leftShoulderPitch = 0.0;
+				lowerArm.leftShoulderRoll = -1.0;
+				lowerArm.rightShoulderRoll = -1.0;
+			}
+			// right arm
+			else
+			{
+				raiseArm.rightShoulderPitch = 2.3;
+				raiseArm.rightShoulderRoll = -1.0;
+				raiseArm.leftShoulderRoll = -1.0;
+				waveArm.rightShoulderPitch = 2.3;
+				waveArm.rightShoulderRoll = -0.5;
+				waveArm.leftShoulderRoll = -1.0;
+				waveArmBack.rightShoulderPitch = 2.3;
+				waveArmBack.rightShoulderRoll = -1.0;
+				waveArmBack.leftShoulderRoll = -1.0;
+				lowerArm.rightShoulderPitch = 0;
+				lowerArm.rightShoulderRoll = -1.0;
+				lowerArm.leftShoulderRoll = -1.0;
+			}
 
-		addPose (raiseArm);
-		addPose (waveArm);
-		addPose (waveArmBack);
-		addPose (waveArm);
-		addPose (waveArmBack);
-		addPose (lowerArm);
-	}
+			raiseArm.speed = 1.0;
+			waveArm.speed = 0.25;
+			waveArmBack.speed = 0.25;
+			lowerArm.speed = 1.0;
+
+			addPose (raiseArm);
+			addPose (waveArm);
+			addPose (waveArmBack);
+			addPose (waveArm);
+			addPose (waveArmBack);
+			addPose (lowerArm);
+		}
 };
 
 #endif
--- joint_controller/include/joint_values.h
+++ joint_controller/include/joint_values.h
@@ -5,6 +5,7 @@ struct joint_values
 {
 	std::vector <std::string> jointNames = {"l_shoulder_pitch", "l_shoulder_roll", "r_shoulder_pitch", "r_shoulder_roll", "waist_pitch"};
 	// maybe get current joint values instead of resetting to 0?
+	// or maybe joints will keep their position if the goal has an empty value for them?
 	float leftShoulderPitch = 0.0;
 	float leftShoulderRoll = 0.0;
 	float rightShoulderPitch = 0.0;
